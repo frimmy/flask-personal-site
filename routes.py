0ad8ba4fcae0e240ba128360b2bb85c16f32046c
adrian.frimpong <adrian.frimpong@gmail.com>
2014-04-09 04:57:31 -0400

    completed shitty personal website

--- routes.py
+++ routes.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+	return render_template('home.html')
+
+@app.route('/about')
+def about():
+	return render_template('About.html')
+
+@app.route('/comedy')
+def comedy():
+	return render_template('comedy.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
